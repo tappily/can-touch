0226ac5c49bf9e3a689146faa10155e162678ad8
tappily <tappily.b@gmail.com>
2014-01-18 17:01:00 -0800

    Using map instead of model for touch model. Reformating code.

--- src/js/can-touch/model.js
+++ src/js/can-touch/model.js
@@ -1,58 +1,59 @@
-define(['can/model', 'can/map/attributes'], function(m) {
-  'use strict';
-  return m.extend({
-    attributes: {
-      phase: 'phase',
-      x: 'number',
-      y: 'number',
-      implementsTouch: 'boolean'
-    },
-    convert: {
-      phase: function(i) {
-        return i;
-      }
-    }
-  }, {
-    start: function(event) {
-      this.removeAttr('x');
-      this.removeAttr('y');
-      this.attr('xs', event.pageX || event.clientX);
-      this.attr('ys', event.pageY || event.clientY);
-    },
-    move: function(event) {
-      this.attr('x', event.pageX);
-      this.attr('y', event.pageY);
-    },
-    update: function(event) {
-      console.log(event.type);
-      switch(event.type) {
-        case 'touchstart':
-          this.start(event.originalEvent.touches[0]);
-          this.attr('phase', 'start');
-          break;
-        case 'mousedown':
-          this.start(event);
-          this.attr('phase', 'start');
-          break;
-        case 'touchend':
-          this.attr('phase', 'end');
-          break;
-        case 'mouseup':
-          this.attr('phase', 'end');
-          break;
-        case 'touchmove':
-          this.move(event.originalEvent.changedTouches[0]);
-          this.attr('phase', 'move');
-          break;
-        case 'mousemove':
-          this.move(event);
-          this.attr('phase', 'move');
-          break;
-        case 'touchcancel':
-          //if this options endOnCancel
-          this.attr('phase', 'cancel');
-          break;
-      }
-    }
-  });
+define(['can/map', 'can/map/attributes'], function (m) {
+    'use strict';
+    return m.extend({
+        attributes: {
+            phase: 'phase',
+            x: 'number',
+            y: 'number',
+            implementsTouch: 'boolean'
+        },
+        convert: {
+            phase: function (i) {
+                return i;
+            }
+        }
+    }, {
+        start: function (touch) {
+            this.removeAttr('x');
+            this.removeAttr('y');
+            console.log('identifier', touch.identifier);
+            this.attr('xs', touch.pageX || touch.clientX);
+            this.attr('ys', touch.pageY || touch.clientY);
+        },
+        move: function (event) {
+            this.attr('x', event.pageX);
+            this.attr('y', event.pageY);
+        },
+        update: function (event) {
+
+            switch (event.type) {
+                case 'touchstart':
+                    this.start(event.originalEvent.touches[0]);
+                    this.attr('phase', 'start');
+                    break;
+                case 'mousedown':
+                    this.start(event);
+                    this.attr('phase', 'start');
+                    break;
+                case 'touchend':
+                    this.attr('phase', 'end');
+                    break;
+                case 'mouseup':
+                    this.attr('phase', 'end');
+                    break;
+                case 'touchmove':
+                    this.move(event.originalEvent.changedTouches[0]);
+                    this.attr('phase', 'move');
+                    break;
+                case 'mousemove':
+                    this.move(event);
+                    this.attr('phase', 'move');
+                    break;
+                case 'touchcancel':
+                    //if this options endOnCancel
+                    this.attr('phase', 'cancel');
+                    break;
+            }
+        }
+    });
 });
